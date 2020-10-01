1a085560b8ab864bb45be6bacaa39e264fdb94fe
Omar Tecpa <omartecpa@gmail.com>
2020-10-01 18:15:04 -0500

    Basic Rock Script

---
+++ Assets/Scripts/Rock.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //Create rock and add force and direction to it.
+    void Start()
+    {
+        // apply impulse force to get game object moving
+        const float MinImpulseForce = 3f;
+        const float MaxImpulseForce = 6f;
+
+        //Create a random angle 
+        float angle = Random.Range(0, 2 * Mathf.PI);
+
+        //Create a direction
+        Vector2 direction = new Vector2( Mathf.Cos(angle), Mathf.Sin(angle));
+        
+        //Create a magnitude based on the min and max impuse
+        float magnitude = Random.Range(MinImpulseForce, MaxImpulseForce);
+        
+        //add Force based on the direction and magnitude
+        GetComponent<Rigidbody2D>().AddForce(
+            direction * magnitude,
+            ForceMode2D.Impulse);
+    }
+
+    // Destroy the rock if out of sight
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/RockSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockSpawner : MonoBehaviour
+{
+
+    //prefab of rock
+    [SerializeField]
+    GameObject rockPrefab;
+
+
+    //List of Rock Sprites
+    [SerializeField]
+    List<Sprite> sprites;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
