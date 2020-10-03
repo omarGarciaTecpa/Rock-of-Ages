645789ebd7c2d5448b27be24a4c8d61602bf7b1a
omartecpa <omartecpa@gmail.com>
2020-10-03 08:39:49 -0500

    Added Timer

--- Assets/Scripts/RockSpawner.cs
+++ Assets/Scripts/RockSpawner.cs
@@ -15,16 +15,30 @@ public class RockSpawner : MonoBehaviour
     [SerializeField]
     List<Sprite> sprites;
 
+    Timer spawnTimer;
+
+    [SerializeField]
+    float spawnSeconds;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Initialize timer
+        spawnTimer = gameObject.AddComponent<Timer>();
+        spawnTimer.Duration = spawnSeconds;
+        //Start timer
+        spawnTimer.Run();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (spawnTimer.Finished) {
+            //Spawner time is finished
+            print("Time is up, restarting");
+            spawnTimer.Run();
+        }
     }
 }
